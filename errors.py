afe576ae89d9a706bf92967146c66ddb8b62dd6e
mikeplavsky <mike.plavsky@quest.com>
2016-08-05 14:44:10 -0400

    decorators and coroutines

---
+++ errors.py
@@ -0,0 +1,38 @@
+import asyncio
+
+def log(func):
+
+    print("just returning it")
+
+    def log_func(*x,**k):
+
+        try:
+
+            print("calling...")
+            return func(*x,**k)
+
+        except Exception:
+            print("Ups! Got an error")
+
+    return log_func
+
+@log
+def sum(x,y):
+    raise Exception("Wow!")
+    return x + y
+
+@log
+async def error():
+
+    print("I am here!")
+    raise Exception("it is an error")
+
+async def check():
+    await error()
+
+def main():
+
+    loop = asyncio.new_event_loop()
+    loop.run_until_complete(check())
+
+    loop.close()
