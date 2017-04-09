b4a3085f680d5b5f6b51b64c7562fdcac8aa4add
mikeplavsky <mikhail.plavskiy@quest.com>
2017-04-09 11:36:32 +0300

    added bad web server

---
+++ Dockerfile
@@ -0,0 +1,5 @@
+FROM python:latest
+
+RUN pip install aiohttp
+RUN pip install flask
+
---
+++ nice.py
@@ -0,0 +1,19 @@
+from aiohttp import web
+
+async def do(req):
+
+   import time
+   time.sleep(10)
+
+   return web.Response(text="Done\n")
+
+async def handle(req):
+    return web.Response(text="Wow\n") 
+
+app = web.Application()
+
+app.router.add_get("/",handle)
+app.router.add_get("/do",do)
+
+web.run_app(app,host="0.0.0.0",port=8080)
+
